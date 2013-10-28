33080f785d947ec83995692776ee5cbbb77c9bc1
cashwu <cash@cashwu.com>
2013-10-29 00:17:10 +0800

    FirstCommit

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class PersonData
+    {
+        public string Name { get; set; }
+
+        public int HP { get; set; }
+    }
+
+    /// <summary>
+    /// http://www.dotblogs.com.tw/hatelove/archive/2011/12/10/template-method-by-interface.aspx
+    /// </summary>
+    public abstract class ConfigParser
+    {
+        protected PersonData mPData = null;
+
+        public PersonData doParse()
+        {
+            readData();
+            parseToken();
+            buildModel();
+            validate();
+            return mPData;
+        }
+
+        protected virtual void readData()
+        {
+        }
+
+        protected virtual void parseToken()
+        {
+        }
+
+        abstract protected void buildModel();
+
+        protected virtual void validate()
+        {
+        }
+    }
+
+    public class FileConfigParser : ConfigParser
+    {
+        private String mFileName = null;
+
+        public FileConfigParser(string fileName)
+        {
+            this.mFileName = fileName;
+        }
+
+        protected override void buildModel()
+        {
+            mPData = new PersonData();
+            mPData.Name = "Cash";
+            mPData.HP = 100;
+        }
+    }
+
+    public class DBConfigParse : ConfigParser
+    {
+        private String mFileName = null;
+
+        public DBConfigParse(string fileName)
+        {
+            this.mFileName = fileName;
+        }
+
+        protected override void buildModel()
+        {
+            mPData = new PersonData();
+            mPData.Name = "Test";
+            mPData.HP = 80;
+        }
+    }
+}
---
+++ TemplateMethodTests/ConfigParserTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemplateMethod;
+
+namespace TemplateMethod.Tests
+{
+    [TestClass()]
+    public class ConfigParserTests
+    {
+        [TestMethod()]
+        public void doParseTest()
+        {
+            ConfigParser fileParse = new FileConfigParser("c:\\config.ini");
+            PersonData fpd = fileParse.doParse();
+            Assert.AreEqual("Cash", fpd.Name);
+            Assert.AreEqual(100, fpd.HP);
+
+            ConfigParser dbParse = new DBConfigParse("http://127.0.0.1/hsql/mydb");
+            PersonData dbpd = dbParse.doParse();
+            Assert.AreEqual("Test", dbpd.Name);
+            Assert.AreEqual(80, dbpd.HP);
+        }
+    }
+}
